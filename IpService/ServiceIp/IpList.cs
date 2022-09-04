9a64f85b3c223dec8a7433e2027679b404c9c984
Oğuzhan <49417199+oguzhanyikilmaz@users.noreply.github.com>
2022-09-04 18:28:18 +0300

    Add project files.

---
+++ IpService/ServiceIp/IpList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpService.ServiceIp
+{
+    public class IpList
+    {
+        public string[] WhiteIpList { get; set; }
+    }
+}
---
+++ IpService/ServiceIp/IpMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IpService.ServiceIp
+{
+    public class IpMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IpList _ipList;
+        public IpMiddleware(RequestDelegate next,IOptions<IpList> ipList)
+        {
+            _next = next;
+            _ipList = ipList.Value;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var getIpAddress = httpContext.Connection.RemoteIpAddress;
+            var whiteIpList = _ipList.WhiteIpList.Any(x=>IPAddress.Parse(x).Equals(getIpAddress));
+
+            if (!whiteIpList)
+            {
+                httpContext.Response.StatusCode = (int)StatusCodes.Status403Forbidden;
+                return;
+            }
+            await _next(httpContext);
+        }
+    }
+}
